ed281484ce1301387f7a01e7b55f33c34fd7b35b
alexandre <perso@alexandre-seo.tk>
2020-03-21 13:54:23 +0100

    Finished typescript configuration

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var discord_js_1 = require("discord.js");
-var config = require("./config.json");
-var bot = new discord_js_1.Client();
-bot.on('ready', function () {
-    console.log('Bot is UP !');
-});
-bot.login(config.discordToken);
--- srcs/index.ts
+++ srcs/index.ts
@@ -1,9 +1,10 @@
 import { Client } from 'discord.js'
-import * as config from './config.json'
+import * as config from '../config.json'
 const bot = new Client();
 
 bot.on('ready', () => {
 	console.log('Bot is UP !')
+	bot.user?.setPresence({ activity: { name: 'is under heavy dev' }, status: 'online' })
 })
 
 bot.login(config.discordToken)
