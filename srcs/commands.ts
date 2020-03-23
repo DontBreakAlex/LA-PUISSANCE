2903d92d5d8bfa2c9d605c8e840ec1b9b26edf64
alexandre <perso@alexandre-seo.tk>
2020-03-23 17:50:35 +0100

    Command structure

---
+++ srcs/commands.ts
@@ -0,0 +1,5 @@
+import Calc from './calc';
+
+export default [
+	new Calc()
+]
--- srcs/index.ts
+++ srcs/index.ts
@@ -1,10 +1,25 @@
-import { Client } from 'discord.js'
+import { Client, Message } from 'discord.js'
 import * as config from '../config.json'
+import Commands from './commands'
 const bot = new Client();
 
 bot.on('ready', () => {
 	console.log('Bot is UP !')
 	bot.user?.setPresence({ activity: { name: 'is under heavy dev' }, status: 'online' })
+})
+
+bot.on('message', (message: Message) => {
+	if (message.content.startsWith('lp'))
+	{
+		let array = message.cleanContent.split(' ');
+		array.shift();
+		for (let command of Commands) {
+			if (command.test(array[0])) {
+				command.execute(message, array);
+				break;
+			}
+		}
+	}
 })
 
 bot.login(config.discordToken)
